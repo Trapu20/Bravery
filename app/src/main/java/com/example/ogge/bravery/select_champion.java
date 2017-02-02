95776473e07268ae19c62ac51e1f479251ed3336
Raphael NEMEC <nemecr@edu.htl-villach.at>
2017-02-02 14:53:01 +0100

    Generate Activity

--- app/src/main/java/com/example/ogge/bravery/select_champion.java
+++ app/src/main/java/com/example/ogge/bravery/select_champion.java
@@ -9,5 +9,6 @@ public class select_champion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_champion);
+        //
     }
 }
