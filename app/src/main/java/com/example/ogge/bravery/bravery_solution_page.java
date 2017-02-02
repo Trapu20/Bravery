6d81dd1dd5c370bcf62b9c01d2c52c9ea3459f78
Raphael NEMEC <nemecr@edu.htl-villach.at>
2017-02-02 14:50:58 +0100

    Generate Activity

---
+++ app/src/main/java/com/example/ogge/bravery/bravery_solution_page.java
@@ -0,0 +1,13 @@
+package com.example.ogge.bravery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class bravery_solution_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bravery_solution_page);
+    }
+}
---
+++ app/src/main/java/com/example/ogge/bravery/select_champion.java
@@ -0,0 +1,13 @@
+package com.example.ogge.bravery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class select_champion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_champion);
+    }
+}
